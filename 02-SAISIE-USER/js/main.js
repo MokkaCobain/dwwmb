80487b101d97a7f4cba0a393edba37bea6aac7f6
MokkaCobain <91149724+MokkaCobain@users.noreply.github.com>
2022-03-09 15:19:04 +0100

    Modification 02-main.js

--- 02-SAISIE-USER/js/main.js
+++ 02-SAISIE-USER/js/main.js
@@ -2,24 +2,33 @@
  * Les différentes boites de dialogues en js 
  ********************************************/
 
+/* Affichage dans la console */
 
-// let warning = window.warn("ceci est une boite de ?");
-// let erreur = window.error("Cecie est une boite d'erreur");
-// let alerte = window.alert("ceci est une boite d'alerte");
-// let reponse = window.confirm("ceci est une boite de dialogue avec le user avec boutons");
-// let saisie = prompt("Ceci est une boite pour dialoguer avec l'user avec champ input type string");
+console.warn("ceci est une boite d'avertissement");
+console.error("Cecie est une boite d'erreur");
+
+/* Affichage dans navigateur */
+
+alert("ceci est une boite d'alerte");
+let reponse = confirm("ceci est une boite de dialogue avec le user avec boutons");
+console.log(reponse);
+
+let saisie = prompt("Ceci est une boite pour dialoguer avec l'user avec champ input type string");
+console.log(saisie);
+console.log(typeof saisie);
 
 /********************************************
  * Pour utiliser des strings sans se faire chier 
  ********************************************/
 
-console.log(`Dimanche c'est "pépouze"`); // le template string ` ... ` c'est chouette
+console.log(`Dimanche c'est "pépouze"`); 
+/* le template string ` ... ` c'est chouette */
 
 /********************************************
  *  Pour transformer un string en number 
  ********************************************/
 
-// let nombre = Number(prompt("Cette réponse sera un nombre"));
+let nombre = Number(prompt("Cette réponse sera un nombre"));
 
 /********************************************
  * Récupérer la saisie user 
